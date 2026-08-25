bded753c990bcc06a16d4dc3d72989b31793b38f
Rohit Kumar Modi <modiaarav777@gmail.com>
2026-08-25 15:09:27 +0530

    finding triplet element which sum equals to zero
    Implement brute force method to find triplets that sum to zero.

---
+++ Array/TripletSum.java
@@ -0,0 +1,20 @@
+class TripletSum{
+  
+  public static void main(String argss[]){
+    
+    int arr[] = {-1,0,1,2,1};
+    //  We have to find those three element which sum will be equal to 0;
+    // Bruit Force => T.C =O(N^3)
+
+    for(int i =0;i<arr.length-2;i++){
+       for(int j =i+1;j<arr.length-1;j++){
+           for(int k =i+2;k<arr.length;k++){
+                 if(arr[i] + arr[j] + arr[k] ==0){
+                     System.out.println("Triplet sum :" + arr[i]+" "+arr[j]+" "+arr[k]);
+                     break;
+                 }
+             }
+         }
+      }
+    }
+}
