cf2f7fabef8ddadc75bc6733d2b801fe25def06c
Rohit Kumar Modi <modiaarav777@gmail.com>
2026-09-08 22:56:43 +0530

    Implement sliding window techniques in Java

---
+++ SlidingWindowTemplate.java
@@ -0,0 +1,32 @@
+class SlidingWindow{
+  public static void fixedSizeWidow(int arr[]){
+    int sum = 0;
+    int ans = 0;
+    for(int i = 0; i < arr.length; i++) {
+      sum += arr[i];   // Current element window me add karo // Jab window size k ho jaye
+      if(i >= k - 1) {  // Jab window size k ho jaye
+        ans = Math.max(ans, sum);  // Answer update karo
+        sum -= arr[i - k + 1];    // Window ka first element hatao
+    }
+}
+    public static int VariableSizeWindow(int arr[]){
+      int left = 0;
+      int sum = 0;
+      int ans = 0;
+      for(int right = 0; right < arr.length; right++) {
+        sum += arr[right];   // Right wala element add karo
+        while(sum > target) {     // Jab condition kharab ho jaye  
+        sum -= arr[left];    // Left wala element hatao 
+        left++;   // Left ko aage badhao
+    }
+    ans = Math.max(ans, right - left + 1);   // Ab current valid window ka answer check karo
+    }
+  }
+    
+  public static void main(String args[]){
+    int arr [] ={};
+    int k;
+    fixedSizeWindow(arr, k);
+    variableSizeWindow(int arr[]);
+  }
+}
